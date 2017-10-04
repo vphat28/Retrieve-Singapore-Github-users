b6ecb4135b889ea258afee633522b41c7837cc24
Xavi <vphat28@gmail.com>
2017-10-05 00:12:32 +0700

    Implement API call

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+define('TOKEN', 'vphat28:6dabe48cf6003c5e21584c1c987b50f9d8b6bc84');
+
+function githubRequest($url)
+{
+    $ch = curl_init();
+	
+    // Token can be get from here https://github.com/settings/tokens
+    $access = TOKEN;
+    curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_USERAGENT, 'website');
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_USERPWD, $access);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    $result = json_decode(trim($output), true);
+	
+    return $result;
+}
+
+$result = githubRequest('https://api.github.com/search/users?q=location:singapore');
+$total = $result['total_count'];
+$result = githubRequest('https://api.github.com/search/users?q=location:singapore&per_page=' . $total . '&page=1');
+
+echo "<pre>";
+print_r($result);
+echo "</pre>";
